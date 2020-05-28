666117c174caa6bce3bd3b09aa39cdf906ad8718
Mariann Sarkadi <v-msarkadi@hotels.com>
2020-05-28 18:05:59 +0200

    initial setup

---
+++ client/src/app/components/app.component.js
@@ -0,0 +1,9 @@
+angular.
+    module('countrySearch').
+    component('appComponent', {
+        templateUrl: 'components/app.component.html',
+        controller: function GreetUserController() {
+            console.log('Hello World')
+        }
+    }
+);
